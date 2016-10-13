10c68dea28c58e7a637f6a9b77208972a4a7f596
hinimix <hinimix.guan@inspiry.cn>
2016-10-13 23:39:25 +0800

    update nexus3
    Signed-off-by: hinimix <hinimix.guan@inspiry.cn>

--- bash/create_nexus3.0.sh
+++ bash/create_nexus3.0.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-apt install maven
-
-if [ $? -ne 0 ]; then
-	exit 1
+if [ $UID -ne 0 ]; then
+        echo "this performance need root privileges to excute"
+        exit 1
 fi
+
+if [ -e /etc/redhat-release ]; then
+        yum install maven -y
+else
+        apt install maven -y
+fi
+
 useradd -m nexus
 
-tar xf jdk1.8.tar.gz -C /usr/local/src
-ln -s /usr/local/src/jdk1.8.0_101 /usr/local/jdk
+java -version
+if [ $? -ne 0 ]; then
+        wget http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz
+        tar xf jdk-8u101-linux-x64 -C /usr/local/src
+        ln -s /usr/local/src
 
-cat >> /etc/profile << EOF
+        cat >> /etc/profile << EOF
         export JAVA_HOME=/usr/local/jdk
         export JRE_HOME=/usr/local/jdk/jre
         export PATH=\$PATH:\$JAVA_HOME/bin:\$JRE_HOME/bin
 EOF
+        source /etc/profile
+fi
 
-source /etc/profile
-
+wget http://download.sonatype.com/nexus/oss/nexus-3.0.1-01-unix.tar.gz
 tar xf nexus-3.0.1-01-unix.tar.gz -C /usr/local/src
 ln -s /usr/local/src/nexus-3.0.1-01 /usr/local/nexus
+
 
 mkdir /usr/local/nexus/data/log
 chown -R nexus:nexus /usr/local/src/nexus-3.0.1-01
